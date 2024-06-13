2dbd109ce73f279fb2e418f80c4debe933ebca43
YoonseolLee <thisyoon97@gmail.com>
2024-06-13 16:55:11 +0900

    fix: Fix LogInterceptor error

--- src/main/java/com/sportsLog/sportsLog/interceptor/LogInterceptor.java
+++ src/main/java/com/sportsLog/sportsLog/interceptor/LogInterceptor.java
@@ -72,7 +72,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 @Component
 public class LogInterceptor implements HandlerInterceptor {
@@ -80,14 +79,13 @@ public class LogInterceptor implements HandlerInterceptor {
 	public static final String LOG_ID = "logId";
 	public static final String START_TIME = "startTime";
 
-
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
 		Exception {
 		String requestURI = request.getRequestURI();
 		String uuid = UUID.randomUUID().toString();
-
 		request.setAttribute(LOG_ID, uuid);
+		request.setAttribute(START_TIME, System.currentTimeMillis());
 
 		if (handler instanceof HandlerMethod) {
 			HandlerMethod hm = (HandlerMethod)handler;
@@ -100,20 +98,25 @@ public class LogInterceptor implements HandlerInterceptor {
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 		ModelAndView modelAndView) throws Exception {
-
-		// 컨트롤러 실행 후, 뷰 렌더링 전에 실행되는 메서드. ModelAndView 객체를 로그로 출력
 		log.info("postHandler [{}]", modelAndView);
 	}
 
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+		Exception ex) throws Exception {
 		String requestURI = request.getRequestURI();
 		Object logId = request.getAttribute(LOG_ID);
-		long startTime = (Long) request.getAttribute(START_TIME);
-		long endTime = System.currentTimeMillis();
-		long duration = endTime - startTime;
+		Object startTimeAttr = request.getAttribute(START_TIME);
 
-		log.info("RESPONSE [{}][{}][{}] - Duration: {} ms", logId, requestURI, handler, duration);
+		if (logId != null && startTimeAttr != null) {
+			long startTime = (Long)startTimeAttr;
+			long endTime = System.currentTimeMillis();
+			long duration = endTime - startTime;
+
+			log.info("RESPONSE [{}][{}][{}] - Duration: {} ms", logId, requestURI, handler, duration);
+		} else {
+			log.warn("RESPONSE [{}][{}][{}] - Missing logId or startTime", logId, requestURI, handler);
+		}
 
 		if (ex != null) {
 			log.error("afterCompletion error!!", ex);
